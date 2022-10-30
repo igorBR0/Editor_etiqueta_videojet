8f876000c02a22be339f5468e3a796e0820da491
IGOR BARBOSA <77306197+armagripada@users.noreply.github.com>
2022-10-30 08:25:20 -0300

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,8 @@ var express = require('express'),
   app = express();
 const port = process.env.PORT || 3000;
 app.use(express.static(__dirname + '/public'), bodyParser.json());
-app.set('views', __dirname + '/');
+
+app.set('views', path.join(__dirname, '/'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
